e43f93c03b5bc79aba3f869e85c27aef23b1c79a
AnkitHire <65785476+AnkitHire@users.noreply.github.com>
2020-07-15 12:48:42 +0530

    Add files via upload

---
+++ Day2/first.js
@@ -0,0 +1,24 @@
+
+console.log("it is printed on console");
+console.warn("it is printed in warning of console");
+console.error("printed in error");
+
+var a =1;
+
+const c=3;
+console.log("a-var",a);
+
+{
+    let b =5
+    console.log("b-let",b);
+}
+//console.log("b",b);
+console.log("c-const",c);
+//c=10;
+var b="aaaa"
+var d=true;
+var e=null;
+console.log("number",a);
+console.log("String",b);
+console.log("boolean",d);
+console.log("null",e);
